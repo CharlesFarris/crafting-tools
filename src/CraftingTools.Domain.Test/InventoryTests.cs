243070c5a6a50fb76212645edbdbfdd0d0f36c91
Charles Farris <charlesfarris71@gmail.com>
2022-03-20 10:16:53 -0600

    Start Inventory implementation

---
+++ src/CraftingTools.Domain.Test/InventoryTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CraftingTools.Domain.Test;
+
+/// <summary>
+/// Tests for <see cref="Inventory"/>.
+/// </summary>
+internal static class InventoryTests
+{
+    /// <summary>
+    /// Validates the behavior of the <c>From</c> method.
+    /// </summary>
+    [Test]
+    public static void From_ValidatesBehavior()
+    {
+        // use case: null slots
+        {
+            var inventory = Inventory.From(slots: default);
+            Assert.That(inventory, Is.EqualTo(Inventory.Empty));
+        }
+    }
+}
---
+++ src/CraftingTools.Domain/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+namespace CraftingTools.Domain;
+
+public sealed class Inventory
+{
+    private Inventory(ImmutableList<(Item, int)> slots)
+    {
+        this.Slots = slots;
+    }
+
+    public ImmutableList<(Item, int)> Slots { get; }
+
+    public static readonly Inventory Empty = new(ImmutableList<(Item, int)>.Empty);
+
+    public static Inventory From(IEnumerable<(Item, int)>? slots)
+    {
+        if (slots is null)
+        {
+            return Inventory.Empty;
+        }
+
+        var validSlots = slots
+            .GroupBy(tuple => tuple.Item1.Id)
+            .Select(group => (group.First().Item1, group.Sum(slot => Math.Max(slot.Item2, val2: 0))))
+            .ToImmutableList();
+        return new Inventory(validSlots);
+    }
+}

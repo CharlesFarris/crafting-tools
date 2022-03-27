5010bba18b30ee54334535d1f36947ab5a4c232f
Charles Farris <charlesfarris71@gmail.com>
2022-03-27 00:52:25 -0600

    Add props file
    - Update projects
    - Update using directives

--- src/CraftingTools.Common.Test/GuidExtensionsTests.cs
+++ src/CraftingTools.Common.Test/GuidExtensionsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SleepingBearSystems.Tools.Railway;
 
 namespace CraftingTools.Common.Test;
--- src/CraftingTools.Domain.Test/InventorySlotTests.cs
+++ src/CraftingTools.Domain.Test/InventorySlotTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Serilog;
 using SleepingBearSystems.Tools.Testing;
 

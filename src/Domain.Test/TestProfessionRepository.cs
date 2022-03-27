838f0d30fc92000af999e43a0f0fe7180daa8ceb
Charles Farris <charlesfarris71@gmail.com>
2022-03-27 12:44:51 -0600

    Simplify test data generation

--- src/Domain.Test/TestProfessionRepository.cs
+++ src/Domain.Test/TestProfessionRepository.cs
@@ -17,14 +17,11 @@ public class TestProfessionRepository : IProfessionRepository
         return Professions.Values.ToImmutableList();
     }
 
-    private static Profession CreateProfession(string id, string name) =>
-        Profession.FromParameters(new Guid(id), name).Unwrap();
-
     private static readonly ImmutableDictionary<Guid, Profession> Professions = new[]
         {
-            CreateProfession(id: "362B9515-7A5C-44B7-9708-A0FB6E48F5B5", name: "Cook"),
-            CreateProfession(id: "C416A1F5-9BCF-4F6D-B6EB-A74FE88EF6AC", name: "Chemist"),
-            CreateProfession(id: "685EDF25-910A-49FD-8700-E04C0C19460F", name: "Blacksmith")
+            Profession.FromParameters(id: "362B9515-7A5C-44B7-9708-A0FB6E48F5B5", name: "Cook").Unwrap(),
+            Profession.FromParameters(id: "C416A1F5-9BCF-4F6D-B6EB-A74FE88EF6AC", name: "Chemist").Unwrap(),
+            Profession.FromParameters(id: "685EDF25-910A-49FD-8700-E04C0C19460F", name: "Blacksmith").Unwrap()
         }
         .ToImmutableDictionary(p => p.Id, p => p);
 }
